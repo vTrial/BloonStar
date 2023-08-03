5757421bd5258dc414dcdab46d3f6f0db843156e
Sean Lam <orange75100@gmail.com>
2023-08-03 14:18:34 -0700

    database now works with flask

--- backend/matches_db.py
+++ backend/matches_db.py
@@ -1,17 +1,22 @@
 import requests
 import db_fns
+import sqlite3 as sql
 import time
 
-# cols:
-# UserId, OppId, Map,Gametype, UserQuad, OppQuad, UserOutcome, Duration, EndRound, 
-
-def createMatchesDb(cursor):
+def createMatchesDb():
+    conn = sql.connect(db_fns.get_b2_db_filepath())
+    cursor = conn.cursor()
     cursor.execute((
         "CREATE TABLE IF NOT EXISTS "
         "Matches(UserId, OppId, Map, Gametype, UserTrio, UserHero, OppTrio, OppHero, UserOutcome, Duration, EndRound)"
     ))
+    conn.commit()
+    cursor.close()
+    conn.close()
 
-def fillMatchesDb(cursor, connection):
+def fillMatchesDb():
+    conn = sql.connect(db_fns.get_b2_db_filepath())
+    cursor = conn.cursor()
     user_ids = cursor.execute("SELECT UserId from Players").fetchall()
     for user_id in user_ids:
         user_id = user_id[0]
@@ -47,12 +52,6 @@ def fillMatchesDb(cursor, connection):
             cursor.execute(insert_query, insert_vals)
         print(user_id)
         time.sleep(0.6)
-    connection.commit()
-
-b2_db_connection = db_fns.b2_db_connection
-b2_db_cursor = db_fns.b2_db_cursor
-
-createMatchesDb(b2_db_cursor)
-fillMatchesDb(b2_db_cursor, b2_db_connection)
-# res = b2_db_cursor.execute("SELECT UserId from Matches")
-# print(res.fetchall())
+    conn.commit()
+    cursor.close()
+    conn.close()
