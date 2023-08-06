bb032f16ec92b330326d4595627c78a26f741e33
vTrial <orange75100@gmail.com>
2023-08-05 21:46:46 -0700

    upgraded towers table

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,23 +1,14 @@
 <script>
-	const get_fetch = async (url) => {
-		const res = await fetch(url);
-		if (res.ok) {
-			return await res.json();
-		} else {
-			throw new Error('Request failed');
-		}
-	};
-	const towers_data_promise = get_fetch('http://localhost:5000/towers/get');
+	import TowersData from './towersData.svelte';
 </script>
 
 <table id="table-container">
-	{#await towers_data_promise then towers_data}
-		{#each Object.entries(towers_data) as [tower, data]}
-			<tr>
-				<td>{tower}</td>
-				<td>{data['games']}</td>
-				<td>{data['wins']}</td>
-			</tr>
-		{/each}
-	{/await}
+	<tr>
+		<th>Tower name</th>
+		<th>Matches</th>
+		<th>Wins</th>
+		<th>Use rate</th>
+		<th>Win rate</th>
+	</tr>
+	<TowersData />
 </table>
