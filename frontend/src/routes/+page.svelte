15a67833221cdd503d100758d15a7b360473e2ee
vTrial <orange75100@gmail.com>
2023-08-05 20:56:38 -0700

    no more tower list

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -7,40 +7,16 @@
 			throw new Error('Request failed');
 		}
 	};
-	const towers = [
-		'DartMonkey',
-		'BoomerangMonkey',
-		'BombShooter',
-		'TackShooter',
-		'IceMonkey',
-		'GlueGunner',
-		'SniperMonkey',
-		'MonkeySub',
-		'MonkeyBuccaneer',
-		'MonkeyAce',
-		'HeliPilot',
-		'MortarMonkey',
-		'DartlingGunner',
-		'WizardMonkey',
-		'SuperMonkey',
-		'NinjaMonkey',
-		'Alchemist',
-		'Druid',
-		'BananaFarm',
-		'SpikeFactory',
-		'MonkeyVillage',
-		'EngineerMonkey'
-	];
 	const towers_data_promise = get_fetch('http://localhost:5000/towers/get');
 </script>
 
 <table id="table-container">
 	{#await towers_data_promise then towers_data}
-		{#each towers as tower}
+		{#each Object.entries(towers_data) as [tower, data]}
 			<tr>
 				<td>{tower}</td>
-				<td>{towers_data[tower]['games']}</td>
-				<td>{towers_data[tower]['wins']}</td>
+				<td>{data['games']}</td>
+				<td>{data['wins']}</td>
 			</tr>
 		{/each}
 	{/await}
