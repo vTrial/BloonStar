547161cba49b2a1fd53b200bab813a9fdaf6b324
vTrial <orange75100@gmail.com>
2023-08-07 13:07:31 -0700

    added map filter

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,6 +1,36 @@
 <script>
-	import TableStatsWhole from './TableStatsWhole.svelte';
+  import FilterDropdown from "./FilterDropdown.svelte"
+  import Nav from "./Nav.svelte"
+  import TableStatsWhole from "./TableStatsWhole.svelte"
+
+  const mapsFilters = [
+    ["", "All Maps"],
+    ["banana_depot_scene", "Banana Depot"],
+    ["basalt_columns", "Basalt Columns"],
+    ["building_site_scene", "Building Site"],
+    ["bloon_bot_factory", "Bloon Bot Factory"],
+    ["castle_ruins", "Castle Ruins"],
+    ["cobra_command", "Cobra Command"],
+    ["dino_graveyard", "Dino Graveyard"],
+    ["garden", "Garden"],
+    ["glade", "Glade"],
+    ["inflection", "Inflection"],
+    ["koru", "Koru"],
+    ["oasis", "Oasis"],
+    ["ports", "Ports"],
+    ["sands_of_time", "Sands of Time"],
+    ["star", "Star"],
+  ]
+  let selectedMap = ""
+
+  function handleFilterChanged(event) {
+    selectedMap = event.detail
+  }
 </script>
 
-<TableStatsWhole datatype="towers" />
-<TableStatsWhole datatype="heroes" />
+<FilterDropdown
+  on:filterChanged={handleFilterChanged}
+  filterList={mapsFilters}
+/>
+<TableStatsWhole datatype="towers" map={selectedMap} />
+<TableStatsWhole datatype="heroes" map={selectedMap} />
---
+++ frontend/src/routes/FilterDropdown.svelte
@@ -0,0 +1,20 @@
+<script>
+  import { createEventDispatcher } from "svelte"
+  const dispatch = createEventDispatcher()
+  export let filterList = []
+  let selectedFilter = ""
+
+  const changedFilter = () => {
+    dispatch("filterChanged", selectedFilter)
+  }
+</script>
+
+<select
+  class="filter-dropdown"
+  bind:value={selectedFilter}
+  on:change={changedFilter}
+>
+  {#each filterList as filter}
+    <option value={filter[0]}>{filter[1]}</option>
+  {/each}
+</select>
