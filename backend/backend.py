f86940de99835dbcc083371538281c146a001553
vTrial <orange75100@gmail.com>
2023-08-07 01:13:32 -0700

    better endpoints?

--- backend/backend.py
+++ backend/backend.py
@@ -40,8 +40,10 @@ schedule.every(60).minutes.do(update_databases)
 app.add_url_rule('/users', view_func=backend_views.get_users)
 app.add_url_rule('/matches', view_func=backend_views.get_matches)
 app.add_url_rule('/matches/count', view_func=backend_views.get_matches_count)
-app.add_url_rule('/towers/get', view_func=backend_views.get_towers)
-app.add_url_rule('/heroes/get', view_func=backend_views.get_heroes)
+app.add_url_rule('/towers/get/', defaults={'match_map': ""}, view_func=backend_views.get_towers)
+app.add_url_rule('/towers/get/<match_map>', view_func=backend_views.get_towers)
+app.add_url_rule('/heroes/get/', defaults={'match_map': ""}, view_func=backend_views.get_heroes)
+app.add_url_rule('/heroes/get/<match_map>', view_func=backend_views.get_heroes)
 
 if __name__ == '__main__':
     try:
