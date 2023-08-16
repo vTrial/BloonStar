a1167c3bf8991bb673bd0d835b6e98fb320641fc
vTrial <orange75100@gmail.com>
2023-08-15 20:49:57 -0700

    made homepage less sad

---
+++ frontend/src/routes/TableStatsDataSmall.svelte
@@ -0,0 +1,46 @@
+<script>
+  import { onMount, beforeUpdate } from "svelte"
+  export let datatype
+  export let map
+  export let thingNames
+  let endpoint = `http://localhost:5000/${datatype}/get/${map}`
+  let total_games = 1
+  let dataList = []
+  const fetchData = async () => {
+    const response = await fetch(endpoint)
+    const dataJson = await response.json()
+    dataList = Object.entries(dataJson).map(([name, stats]) => ({
+      name,
+      ...stats,
+    }))
+    dataList.sort((a, b) => b.games - a.games)
+    total_games = dataList.reduce((total, data) => total + data.games, 0)
+    if (datatype == "towers") total_games /= 3
+    dataList.splice(5)
+  }
+  const to_percent = (num) => {
+    return (num * 100).toFixed(1) + "%"
+  }
+
+  onMount(() => {
+    fetchData()
+  })
+
+  let prevMap = map
+
+  beforeUpdate(() => {
+    if (map !== prevMap) {
+      prevMap = map
+      endpoint = `http://localhost:5000/${datatype}/get/${map}`
+      fetchData()
+    }
+  })
+</script>
+
+{#if dataList}
+  {#each dataList as data}
+    <div class="table-data">{thingNames[data["name"]]}</div>
+    <div class="table-data">{to_percent(data["games"] / total_games)}</div>
+    <div class="table-data">{to_percent(data["wins"] / data["games"])}</div>
+  {/each}
+{/if}
--- frontend/src/routes/towers/+page.svelte
+++ frontend/src/routes/towers/+page.svelte
@@ -11,5 +11,6 @@
 </script>
 
 <Nav />
+<h1>Top Battles 2 Towers</h1>
 <FilterDropdown on:filterChanged={handleFilterChanged} filterList={mapNames} />
 <TableStatsWhole datatype="towers" map={selectedMap} thingNames={towerNames} />
